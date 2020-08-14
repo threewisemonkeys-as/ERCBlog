695b8e415959e0516278d4165658833f617b4fc0
Abdul Jawad Khan <khanabduljawad@gmail.com>
2020-08-14 20:54:03 +0530

    changes to css and js in hope of implementing dark mode

--- static/js/add-on.js
+++ static/js/add-on.js
@@ -4,3 +4,33 @@
   is an official part of the theme (ex. Pull Requests) should be included in main.js
   and follow the formatting and style given.
 */
+var toggle = document.getElementById("dark-mode-toggle");
+var darkTheme = document.getElementById("dark-mode-theme");
+
+toggle.addEventListener("click", () => {
+    console.log("the button was pressed")
+    if (toggle.className === "fal fa-moon-o") {
+        setTheme("dark");
+    } else if (toggle.className === "fal fa-sun-o") {
+        setTheme("light");
+    }
+});
+
+function setTheme(mode) {
+    if (mode === "dark") {
+        darkTheme.disabled = false;
+        toggle.className = "fal fa-sun-o";
+    } else if (mode === "light") {
+        darkTheme.disabled = true;
+        toggle.className = "fal fa-moon-o";
+    }
+}
+// the default theme is light
+var savedTheme = localStorage.getItem("dark-mode-storage") || "light";
+setTheme(savedTheme);
+
+function setTheme(mode) {
+    localStorage.setItem("dark-mode-storage", mode);
+
+    // same as above
+}
